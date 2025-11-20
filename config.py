b3ea801684aa9618e7a82cbfb4e0d5ca4b891f94
mexanickx <broynikirill@mail.ru>
2025-11-20 13:21:04 +0200

    Update configuration tokens and channel IDs

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-TOKEN = "7890660523:AAFmDfHvAZ_d84vqdicURZNhD-qFcuHldt0" # Токен бота
-CHANNEL_ID = -1002299985903 # Айди канала со ставками
+TOKEN = "8345842327:AAFfVpmSM9-O5BeBvPTrQUX09nJSdhfUdRU" # Токен бота
+CHANNEL_ID = -1002622962197 # Айди канала со ставками
 ADMINS = [6581924705, 7774010496] # Айди админа
 BET_URL = "http://t.me/send?start=IVSEPjgyDkze" # Ссылка на счет
-CRYPTOPAY_TOKEN = "277110:AALlAjbTdnLYxmU9eufTTLFBC89PE1MsXhD" # Токен криптопей
+CRYPTOPAY_TOKEN = "369438:AAEKsbWPZPQ0V3YNV4O0GHcWTvSbzkEar43" # Токен криптопей
 WIN_IMAGE = "win.jpeg" # Название файла с картинкой выигрыша
 LOSE_IMAGE = "lose.jpeg" # Название файла с картинкой проигрыша
-LOGS_ID = -1002480060986 # Айди канала с оплатами
+LOGS_ID = -1002622962197 # Айди канала с оплатами
 RULES_LINK = "https://t.me/+c1Yq3xtf5gQ1YWMy" # Канал с правилами
 NEWS_LINK = "https://t.me/+j6B1t_2amegyNThi" # Новостной канал
 OWNER_LINK = "https://t.me/saporv" # Ссылка на Поддержку/Создателя
 PEREHOD_LINK = "https://t.me/+P0_NIKZ8mTxjNWFi" # Ссылка на переходник
-BETS_LINK = "https://t.me/+8vINJo8LYpZmOTFi" # Ссылка на канал ставок
+BETS_LINK = "https://t.me/usergiveaways" # Ссылка на канал ставок
 CASINO_NAME = "MOFTERBET" # Название казино
 KAK_SDELAT = "https://t.me/+j6B1t_2amegyNThi" # Ссылка как сделать ставку
 KOMANDI_IGRI = "https://t.me/+P0_NIKZ8mTxjNWFi" # Ссылка игр
